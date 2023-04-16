30d26d0506cbdddb4e454885e3d07168e6f70cbd
Itman33 <121342296+Itman33@users.noreply.github.com>
2023-04-16 11:02:22 +0000

    Pending changes exported from your codespace

--- src/main/java/xyz/mathax/mathaxclient/addons/AddonManager.java
+++ src/main/java/xyz/mathax/mathaxclient/addons/AddonManager.java
@@ -1,37 +1,77 @@
-package xyz.mathax.mathaxclient.addons;
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
+ * Copyright (c) Meteor Development.
+ */
 
+package meteordevelopment.meteorclient.addons;
+
+import meteordevelopment.meteorclient.MeteorClient;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.entrypoint.EntrypointContainer;
 import net.fabricmc.loader.api.metadata.ModMetadata;
 import net.fabricmc.loader.api.metadata.Person;
-import xyz.mathax.mathaxclient.MatHax;
-import xyz.mathax.mathaxclient.utils.network.versions.Version;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class AddonManager {
-    public static final List<MatHaxAddon> ADDONS = new ArrayList<>();
+    public static final List<MeteorAddon> ADDONS = new ArrayList<>();
 
     public static void init() {
-        reigsterMatHax();
+        // Meteor pseudo addon
+        {
+            MeteorClient.ADDON = new MeteorAddon() {
+                @Override
+                public void onInitialize() {}
 
-        for (EntrypointContainer<MatHaxAddon> entrypoint : FabricLoader.getInstance().getEntrypointContainers("mathax", MatHaxAddon.class)) {
+                @Override
+                public String getPackage() {
+                    return "meteordevelopment.meteorclient";
+                }
+
+                @Override
+                public String getWebsite() {
+                    return "https://meteorclient.com";
+                }
+
+                @Override
+                public GithubRepo getRepo() {
+                    return new GithubRepo("MeteorDevelopment", "meteor-client");
+                }
+
+                @Override
+                public String getCommit() {
+                    String commit = MeteorClient.MOD_META.getCustomValue(MeteorClient.MOD_ID + ":commit").getAsString();
+                    return commit.isEmpty() ? null : commit;
+                }
+            };
+
+            ModMetadata metadata = FabricLoader.getInstance().getModContainer(MeteorClient.MOD_ID).get().getMetadata();
+
+            MeteorClient.ADDON.name = metadata.getName();
+            MeteorClient.ADDON.authors = new String[metadata.getAuthors().size()];
+            if (metadata.containsCustomValue(MeteorClient.MOD_ID + ":color")) {
+                MeteorClient.ADDON.color.parse(metadata.getCustomValue(MeteorClient.MOD_ID + ":color").getAsString());
+            }
+
+            int i = 0;
+            for (Person author : metadata.getAuthors()) {
+                MeteorClient.ADDON.authors[i++] = author.getName();
+            }
+        }
+
+        // Addons
+        for (EntrypointContainer<MeteorAddon> entrypoint : FabricLoader.getInstance().getEntrypointContainers("meteor", MeteorAddon.class)) {
             ModMetadata metadata = entrypoint.getProvider().getMetadata();
-            MatHaxAddon addon = entrypoint.getEntrypoint();
+            MeteorAddon addon = entrypoint.getEntrypoint();
 
             addon.name = metadata.getName();
 
-            addon.version = new Version(metadata.getVersion().getFriendlyString());
-
-            if (metadata.getAuthors().isEmpty()) {
-                throw new RuntimeException("Addon %s requires at least 1 author to be defined in it's fabric.mod.json. See https://fabricmc.net/wiki/documentation:fabric_mod_json_spec".formatted(addon.name));
-            }
-
+            if (metadata.getAuthors().isEmpty()) throw new RuntimeException("Addon %s requires at least 1 author to be defined in it's fabric.mod.json. See https://fabricmc.net/wiki/documentation:fabric_mod_json_spec".formatted(addon.name));
             addon.authors = new String[metadata.getAuthors().size()];
 
-            if (metadata.containsCustomValue(MatHax.ID + ":color")) {
-                addon.color.parse(metadata.getCustomValue(MatHax.ID + ":color").getAsString());
+            if (metadata.containsCustomValue(MeteorClient.MOD_ID + ":color")) {
+                addon.color.parse(metadata.getCustomValue(MeteorClient.MOD_ID + ":color").getAsString());
             }
 
             int i = 0;
@@ -40,37 +80,6 @@ public class AddonManager {
             }
 
             ADDONS.add(addon);
-        }
-    }
-
-    private static void reigsterMatHax() {
-        MatHax.ADDON = new MatHaxAddon() {
-            @Override
-            public void onInitialize() {}
-
-            @Override
-            public String getPackage() {
-                return "xyz.mathax.mathaxclient";
-            }
-
-            @Override
-            public String getWebsite() {
-                return "https://mathaxclient.xyz";
-            }
-        };
-
-        ModMetadata matHaxMetadata = FabricLoader.getInstance().getModContainer(MatHax.ID).get().getMetadata();
-
-        MatHax.ADDON.name = matHaxMetadata.getName();
-        MatHax.ADDON.version = new Version(matHaxMetadata.getVersion().getFriendlyString());
-        MatHax.ADDON.authors = new String[matHaxMetadata.getAuthors().size()];
-        if (matHaxMetadata.containsCustomValue(MatHax.ID + ":color")) {
-            MatHax.ADDON.color.parse(matHaxMetadata.getCustomValue(MatHax.ID + ":color").getAsString());
-        }
-
-        int i = 0;
-        for (Person author : matHaxMetadata.getAuthors()) {
-            MatHax.ADDON.authors[i++] = author.getName();
         }
     }
 }
